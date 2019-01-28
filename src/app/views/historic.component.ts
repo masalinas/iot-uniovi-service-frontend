7be0ce047b062f688b1be03da059a9b894ffe147
Miguel Salinas Gancedo <miguel@thingtrack.com>
2019-01-28 15:51:02 +0100

    last update

--- src/app/views/historic.component.ts
+++ src/app/views/historic.component.ts
@@ -9,36 +9,40 @@ declare var vis:any;
   styleUrls: ['./historic.component.css']
 })
 export class HistoricComponent implements OnInit {   
-    title = 'Historic Graph';
+  title = 'Historic Graph';
 
-    dateFrom = new FormControl(new Date());
-    dateTo = new FormControl(new Date());
-    serializedDate = new FormControl((new Date()).toISOString());
+  dateFrom = new FormControl(new Date());
+  dateTo = new FormControl(new Date());
+  serializedDate = new FormControl((new Date()).toISOString());
 
-    @ViewChild("historicContainer") container: ElementRef;
+  @ViewChild("historicContainer") container: ElementRef;
 
-    dataset = new vis.DataSet();  
-    graph: any;
+  dataset = new vis.DataSet();  
+  graph: any;
 
-    public ngOnInit(): void {  
-      // configure realtime graph
-    var options = {
-      start: vis.moment().add(-30, 'seconds'), // changed so its faster
-      end: vis.moment(),
-      drawPoints: {
-        style: 'circle' // square, circle
-      },
-      shaded: {
-        orientation: 'bottom' // top, bottom
-      },
-      dataAxis: {
-        left: {title: {
-          text:'Temperature [℃]'}
-        }
+  public ngOnInit(): void {  
+    // configure realtime graph
+  var options = {
+    start: vis.moment().add(-30, 'seconds'), // changed so its faster
+    end: vis.moment(),
+    drawPoints: {
+      style: 'circle' // square, circle
+    },
+    shaded: {
+      orientation: 'bottom' // top, bottom
+    },
+    dataAxis: {
+      left: {title: {
+        text:'Temperature [℃]'}
       }
-    };
-
-    let container = this.container.nativeElement;
-    this.graph = new vis.Graph2d(container, this.dataset, options);   
     }
+  };
+
+  let container = this.container.nativeElement;
+  this.graph = new vis.Graph2d(container, this.dataset, options);   
+  }
+
+  onload(event: any) {
+    
+  }    
 }
