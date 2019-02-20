047a36bdff7b2897bab3085a7e3733c315c857a8
Miguel Salinas Gancedo <miguel@thingtrack.com>
2019-02-20 12:20:08 +0100

    last update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,8 @@ export const environment = {
   production: true,
   basePath: ENV.basePath === '$BASE_PATH' ? 'http://127.0.0.1:3000' : ENV.basePath,
   brokerPath: ENV.brokerPath === '$BROKER_PATH' ? 'http://localhost:8080' : ENV.brokerPath,
+  brokerUsername: ENV.brokerUsername === '$BROKER_USERNAME' ? 'admin' : ENV.brokerUsername,
+  brokerPassword: ENV.brokerPassword === '$BROKER_PASSWORD' ? 'uniovi' : ENV.brokerPassword,
   brokerDeviceTopic: ENV.brokerDeviceTopic === '$BROKER_DEVICE_TOPIC' ? 'uniovi/poc/#' : ENV.brokerDeviceTopic,
   brokerFeedbackTopic: ENV.brokerFeedbackTopic === '$BROKER_FEEDBACK_TOPIC' ? 'uniovi/poc/feedback' : ENV.brokerFeedbackTopic
 };
